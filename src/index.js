81d46c79698d77e0b7211eb7a51bb669ffd455a9
Andreas Grafberger <grafbergerandreas@gmail.com>
2020-09-04 14:32:18 +0200

    refactor entry file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-import { loadComponent } from "./loadComponents";
-
 import ProtvistaProteomicsdb from "./protvista-proteomicsdb";
 
-//import _transformDataFeatureAdapter from "./protvista-uniprot";
-//import _transformDataProteomicsAdapter from "./protvista-uniprot";
-//import _transformDataStructureAdapter from "./protvista-uniprot";
-//import _transformDataVariationAdapter from "./protvista-uniprot";
-//import _transformDataInterproAdapter from "./protvista-uniprot";
-
-//export const transformDataFeatureAdapter = _transformDataFeatureAdapter;
-//export const transformDataProteomicsAdapter = _transformDataProteomicsAdapter;
-//export const transformDataStructureAdapter = _transformDataStructureAdapter;
-//export const transformDataVariationAdapter = _transformDataVariationAdapter;
-//export const transformDataInterproAdapter = _transformDataInterproAdapter;
-
-loadComponent("protvista-proteomicsdb", ProtvistaProteomicsdb);
+if (!window.customElements.get("protvista-proteomicsdb")) {
+    window.customElements.define("protvista-proteomicsdb", ProtvistaProteomicsdb);
+  }
 
 export default ProtvistaProteomicsdb;
